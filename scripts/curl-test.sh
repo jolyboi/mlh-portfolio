ef63ac7fa73bf859753c3bcae5323984dba21e48
Andrei <amorokov003@gmail.com>
2026-07-07 13:39:17 +0500

    rename test script to curl-test.sh

--- scripts/curl-test.sh
+++ scripts/curl-test.sh
@@ -2,25 +2,25 @@
 
 URL="http://localhost:5000/api/timeline_post"
 
-# Make a random post so each run is unique.
+
 NAME="Test User $RANDOM"
 EMAIL="test$RANDOM@example.com"
 CONTENT="Test post $RANDOM"
 
 # Create post with and grab its id
-echo "Creating a post..."
+echo "Creating a post.."
 ID=$(curl -s -X POST "$URL" -d "name=$NAME" -d "email=$EMAIL" -d "content=$CONTENT" | jq '.id')
 echo "Created post with id $ID"
 
-# 2. Check the post shows up in GET.
-echo "Checking the post was added..."
+# Check the post shows up 
+echo "Checking the post was added.."
 if curl -s "$URL" | grep -q "$CONTENT"; then
-    echo "Post was added!"
+    echo "Post was added"
 else
-    echo "Post was NOT found!"
+    echo "Post was not found"
 fi
 
-# 3. Delete the post to clean up.
-echo "Deleting the post..."
+# Delete the post
+echo "Deleting the post.."
 curl -s -X DELETE "$URL/$ID"
 echo
